eba2d2304acd7278d1c20a95be6eef0ba2d1fb25
Bui Phi Quan <rongxanh2052004@yahoo.com>
2014-11-01 15:42:55 +0700

    slot halloween

--- Assets/Scripts/Models/SlotMachine/SlotHalloween/SlotItemHalloween.cs
+++ Assets/Scripts/Models/SlotMachine/SlotHalloween/SlotItemHalloween.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class SlotItemHalloween : SlotItem {
 
-  private string[] spriteNames = new string[10] {"wild", "pumpkin", "bat", "dracula", "frankenstein", "halloween_cat", "hat", "jack_skellington", "scream", "witch"};
-
+  private string[] spriteNames = new string[11] {"wild", "pumpkin", "bat", "dracula", "frankenstein", "halloween_cat", "hat", "jack_skellington", "scream", "witch", "coins"};
+	
   public override string GetSpriteName(int index) {
     return spriteNames[index];
   }
